b85a3b43c341159834a51e732ab34932b09d4112
Jhenrique44 <70330647+Jhenrique44@users.noreply.github.com>
2021-03-15 14:22:53 -0300

    delete

--- demo/src/main/java/com/example/demo/controller/EventController.java
+++ demo/src/main/java/com/example/demo/controller/EventController.java
@@ -9,6 +9,7 @@ import com.example.demo.service.EventService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,5 +52,10 @@ public class EventController {
 
     }
 
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete (@PathVariable Long id ){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
--- demo/src/main/java/com/example/demo/dto/UpdateEventDTO.java
+++ demo/src/main/java/com/example/demo/dto/UpdateEventDTO.java
@@ -2,6 +2,6 @@ package com.example.demo.dto;
 
 public class UpdateEventDTO {
     
-
+    
     
 }
